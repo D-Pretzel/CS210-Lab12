60ee0c34ff3575f3a1981002384cd77cace1a85e
David <petzoldavid02@gmail.com>
2022-09-12 13:36:08 -0600

    Mild reorg

---
+++ Problem3/lab12functs.h
@@ -0,0 +1,11 @@
+/** lab12functs.h
+ * ===========================================================
+ * Name: name, date
+ * Section: your section
+ * Project: assignment information
+ * Purpose: high level description of purpose of the program
+ *          could be multiple lines
+ * =========================================================== */
+
+//numFactorial() calculates the factorial of a number
+int numFactorial(int num);
